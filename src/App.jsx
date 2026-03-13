08a82d3b53e9867d93ae8d62e445626b487c824f
farhan_2312 <farhanmohammed2312@gmail.com>
2026-03-13 14:21:01 +0400

    removed iframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
-import Dashboard from './ME_Logistics_Dashboard.jsx';
+import YourComponent from './ME_Logistics_Dashboard.jsx'; // Update this name
 
 function App() {
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      width: '100%',
-    }}>
-      <Dashboard />
-      <div style={{
-        width: '100%',
-        maxWidth: 1400,
-        padding: '0 20px',
-        marginTop: 24,
-        marginBottom: 32,
-        boxSizing: 'border-box',
-      }}>
-        <iframe
-          src="https://scs.lognai.com"
-          title="SCS LognAI"
-          width="100%"
-          height="800"
-          frameBorder="0"
-          allowFullScreen
-          style={{ borderRadius: 10, border: '1px solid #E2E8F0' }}
-        />
-      </div>
+    <div>
+      <YourComponent />
     </div>
-  );
+  )
 }
 
 export default App;
